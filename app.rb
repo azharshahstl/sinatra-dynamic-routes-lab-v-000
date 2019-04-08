679fdc52a22e76aa92c17f8820ca3280f758ca67
Azhar Shah <azharshahstl@yahoo.com>
2019-04-08 16:15:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @phrase = []
     params.each_value {|value| @phrase << value} 
-    @phrase.join(" ")
-    #binding.pry
+    "#{@phrase.join(" ")}."
+    binding.pry
   end
   
   get '/:operation/:number1/:number2' do 
