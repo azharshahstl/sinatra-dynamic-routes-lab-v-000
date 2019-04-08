8b8fdaafb3f9e515af3a2786fb91e46e92034ef3
Azhar Shah <azharshahstl@yahoo.com>
2019-04-08 15:47:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @phrase = []
     params.each_value {|value| @phrase << value} 
-
-    "#{@phrase}"
+    @phrase.join("")
   end
 
 end
