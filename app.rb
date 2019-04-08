a5fc81c55676c0daf9cd5fe6664b3a03874f9d2a
Azhar Shah <azharshahstl@yahoo.com>
2019-04-08 14:54:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/reversename/:name' do 
-    name_reverse = :name.reverse 
-    "#{name_reverse}" 
+    @name_reverse = :name.reverse 
+    "#{@name_reverse}" 
   end
 
 end
